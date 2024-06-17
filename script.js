b914d1c67f51242528832b5afb5b7618d4468421
Bruno Marangoni <brunofrc10@hotmail.com>
2024-06-17 20:16:09 -0300

    sayu me fez chorar

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const url = "https://script.google.com/macros/s/AKfycbyR9x0JvD-BYHpO-sA4uoODIsbo_cQBCZZsX6k7L6r7NkgKPemQI6lOywVPLWafe-dB4Q/exec"
-    const dados = document.getElementById("formulario").addEventListener("submit", function(event) {
-        event.preventDefault();
-        fetch(url, {method : "post", body : new FormData(dados)})
-        .then(response => {
-            alert("dados enviados", response)
-        })
-        .catch(error => {
-            alert(error)
-        })
-    });
+const url =
+  "https://corsproxy.io/?https://script.google.com/macros/s/AKfycbyR9x0JvD-BYHpO-sA4uoODIsbo_cQBCZZsX6k7L6r7NkgKPemQI6lOywVPLWafe-dB4Q/exec";
+const dados = document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    fetch(url, {
+      method: "post",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ NOME: "Bruno" }),
+    })
+      .then((response) => {
+        alert("dados enviados", response);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+    console.log(new FormData(dados));
+  });
